8f0ece40abe7eb036dc588f3ccf80deea834bb12
fernando.bugni@gmail.com <fernando.bugni@gmail.com@64e07e82-3d9a-f2ca-ba95-d66a7072dd82>
2011-09-07 23:05:33 +0000

    Skip, Seq, If.

--- trunk/tp1/src/PestParser-src/src/budapest/pest/predtocvc3/PredToCVC3Translator.java
+++ trunk/tp1/src/PestParser-src/src/budapest/pest/predtocvc3/PredToCVC3Translator.java
@@ -9,33 +9,36 @@ import budapest.pest.dump.pest.PredPrinter;
 import budapest.util.printer.prec.Brackets;
 
 public class PredToCVC3Translator extends PredPrinter {
-	
+
+	//TODO:Probar en cvc3 la validez e invalidez de las formulas.
+	//07/09/2011 daba valid algo invalid.
+
 	public String visit(BinaryPred n, Void arg) {
 		String result = super.visit(n, arg);
 		result = result.replace("&&", "AND");
 		result = result.replace("||", "OR");
 		return result;
 	}
-	
+
 	public String visit(BooleanLiteralPred n, Void arg) {
 		String result = super.visit(n, arg);
 		result = result.replace("false", "FALSE");
 		result = result.replace("true", "TRUE");
 		return result;
 	}
-	
+
 	public String visit(RelationPred n, Void arg) {
 		String result = super.visit(n, arg);
 		result = result.replace("!=", "/=");
 		return result;
 	}
-	
+
 	public String visit(NotPred n, Void arg) {
 		String result = super.visit(n, arg);
 		result = result.replace("! ", "NOT ");
 		return result;
 	}
-	
+
 	public String visit(QuantifiedPred n, Void arg) {
 		String ret = "";
 		switch(n.type) {
