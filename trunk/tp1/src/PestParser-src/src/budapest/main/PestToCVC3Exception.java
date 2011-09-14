68373035d00ddb83cfd8fcb47d33ffc91a1fb981
fernandopaulovsky@gmail.com <fernandopaulovsky@gmail.com@64e07e82-3d9a-f2ca-ba95-d66a7072dd82>
2011-09-14 15:26:28 +0000

    Un poco de automatizacion. Es necesario importar el cvc3.exe a la carpeta cvc3 para poder correr la app. No lo agregué porque iba a ser muy pesado el proyecto.

---
+++ trunk/tp1/src/PestParser-src/src/budapest/main/PestToCVC3Exception.java
@@ -0,0 +1,11 @@
+package budapest.main;
+
+public class PestToCVC3Exception extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public PestToCVC3Exception(){}
+	public PestToCVC3Exception(String message){
+		super(message);
+	}
+}
