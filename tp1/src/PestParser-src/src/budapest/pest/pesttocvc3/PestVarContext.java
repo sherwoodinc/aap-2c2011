6684ab83240f34b1b1c00103f280734b9c45bbb9
fernando.bugni@gmail.com <fernando.bugni@gmail.com@64e07e82-3d9a-f2ca-ba95-d66a7072dd82>
2011-09-15 19:17:00 +0000

    Context con variables iniciales

--- tp1/src/PestParser-src/src/budapest/pest/pesttocvc3/PestVarContext.java
+++ tp1/src/PestParser-src/src/budapest/pest/pesttocvc3/PestVarContext.java
@@ -1,21 +1,30 @@
-package budapest.pest.vcgenerator;
+package budapest.pest.pesttocvc3;
 
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.Map.Entry;
 
 import budapest.pest.ast.pred.Pred;
+import budapest.pest.predtocvc3.PredVarReplacer;
 
 public class PestVarContext {
 
-	public PestVarContext(PestVarContext parentContext) {
+	public PestVarContext(PestVarContext parentContext, TreeMap<String, String> beginContext) {
+		InitVariables = new TreeMap<String, String>(beginContext);
 		valuations = new TreeMap<String, String>();
 		parent = parentContext;
 	}
 
 	public PestVarContext() {
+		InitVariables = new TreeMap<String, String>();
 		valuations = new TreeMap<String, String>();
 		parent = null;
+	}
+	
+	public PestVarContext(PestVarContext context) {
+		InitVariables = new TreeMap<String, String>(context.InitVariables);
+		valuations = new TreeMap<String, String>(context.valuations);
+		parent = context.parent;
 	}
 	
 	// Obtiene el último nombre asignado a una var
@@ -24,17 +33,18 @@ public class PestVarContext {
 			return valuations.get(varName);
 		} else if (parent != null) {
 			return parent.getVarAssignment(varName);
-		} else
+		} else {
 			throw new Exception("Variable inexistente");
+		}
 	}
 
-	// Genera un nombre nuevo y lo devuelve
-	public String setVarAssignment(String varName) {
-		String newSnapshot = varName + String.valueOf(GlobalID);
-		valuations.put(varName, newSnapshot);
-		GlobalID++;
-		return newSnapshot;
-	}
+    // Genera un nombre nuevo y lo devuelve
+    public String setVarAssignment(String varName) {
+            String newSnapshot = varName + String.valueOf(GlobalID);
+            valuations.put(varName, newSnapshot);
+            GlobalID++;
+            return newSnapshot;
+    }
 
 	// Reemplaza las ocurrencias de cada variable por su
 	// renombre del context, además usando contexts padres
@@ -48,9 +58,9 @@ public class PestVarContext {
 	}
 
 	private TreeMap<String, String> allValuations() {
-		if (parent == null)
+		if (parent == null) {
 			return new TreeMap<String, String>(valuations);
-		else {
+		} else {
 			TreeMap<String, String> ret = parent.allValuations();
 			ret.putAll(valuations);
 			return ret;
@@ -58,6 +68,7 @@ public class PestVarContext {
 	}
 
 	private PestVarContext parent = null;
+	private TreeMap<String, String> InitVariables;
 	private TreeMap<String, String> valuations;
 
 	private static int GlobalID = 0;
